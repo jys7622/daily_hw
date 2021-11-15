7a94fa1740fefced3c4140467f424d3e29526738
jys7622 <jys7622@gmail.com>
2021-11-15 17:41:01 +0900

    moive register code

--- 21_1113/arry_max.js
+++ 21_1113/arry_max.js
@@ -1,28 +1,27 @@
-//최대값 최소값
-// function maxPrint(arry) {
-//   let max = -1;
-//   let min = 1000;
-//   for (let i = 0; i < arry.length; i++) {
-//     if (arry[i] > max) {
-//       max = arry[i];
-//     }
-//     if (arry[i] < min) {
-//       min = arry[i];
-//     }
-//   }
-//   // console.log("Min = " + min + ", Max = " + max)
-//   return [max, min];
-// }
+// 최대값 최소값
+function maxPrint(arry) {
+  let max = -1;
+  let min = 1000;
+  for (let i = 0; i < arry.length; i++) {
+    if (arry[i] > max) {
+      max = arry[i];
+    }
+    if (arry[i] < min) {
+      min = arry[i];
+    }
+  }
+  // console.log("Min = " + min + ", Max = " + max)
+  return [max, min];
+}
 
-//
-// const max = [1, 2, 5, 1, 6, 7, 9];
-// // maxPrint(arry)
-// // console.log(maxPrint(arry))
-// // console.log(maxPrint(arry));
-// // const arry2 = [1231, 12566, 745735, 124];
-// // console.log(maxPrint(arry2));
-// const myMaxNum = maxPrint(max);
-// console.log(myMaxNum);
+const max = [1, 2, 5, 1, 6, 7, 9];
+// maxPrint(arry)
+// console.log(maxPrint(arry))
+// console.log(maxPrint(arry));
+// const arry2 = [1231, 12566, 745735, 124];
+// console.log(maxPrint(arry2));
+const myMaxNum = maxPrint(max);
+console.log(myMaxNum);
 // ----------------------------------------------------------------
 //정해진 범위안에서 숫자 뽑기
 
